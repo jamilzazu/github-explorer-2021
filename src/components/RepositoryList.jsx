837274820b44e01c7f64c4d10a54d6e72a4468df
Jamil Zazu <jamillzazu@gmail.com>
2021-03-27 00:23:34 -0400

    feat: usando useEffect

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import { RepositoryItem } from "./RepositoryItem";
 
 import "../styles/repositories.scss";
@@ -9,13 +10,19 @@ const repository = {
 }
 
 export function RepositoryList() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    fetch('https://api.github.com/users/jamilzazu/repos')
+      .then(response => response.json())
+      .then(data => setRepositories(data))
+  }, [])
+
   return (
     <section className="repository-list">
       <h1>Lista de reopostórios</h1>
 
       <ul>
-        <RepositoryItem repository={repository} />
-        <RepositoryItem repository={repository} />
         <RepositoryItem repository={repository} />
       </ul>
     </section>
